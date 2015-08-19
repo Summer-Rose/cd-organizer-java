58fd6693ebd81a12b42309dc788a77c39810a0d4
Summer Brochtrup & Juliana Suzuki <student@epicodus.com>
2015-08-19 09:45:29 -0700

    add unit tests, velocity templates, and input form

--- src/main/java/CD.java
+++ src/main/java/CD.java
@@ -1,3 +1,23 @@
 public class CD {
+  private String mTitle;
+  private String mArtist;
+  private String mGenre;
 
+  public CD (String title, String artist, String genre){
+    mTitle = title;
+    mArtist = artist;
+    mGenre = genre;
+  }
+
+  public String getTitle(){
+    return mTitle;
+  }
+
+  public String getArtist(){
+    return mArtist;
+  }
+
+  public String getGenre(){
+    return mGenre;
+  }
 }
--- src/test/java/UnitTest.java
+++ src/test/java/UnitTest.java
@@ -3,4 +3,27 @@ import static org.junit.Assert.*;
 
 public class UnitTest {
 
+  @Test
+  public void CD_instantiatesCorrectly() {
+    CD testCD = new CD("The White Album", "The Beatles", "British Rock");
+    assertEquals(true, testCD instanceof CD);
+  }
+
+  @Test
+  public void CD_getsCorrectTitle() {
+    CD testCD = new CD("The White Album", "The Beatles", "British Rock");
+    assertEquals("The White Album", testCD.getTitle());
+  }
+
+  @Test
+  public void CD_getsCorrectArtist() {
+    CD testCD = new CD("The White Album", "The Beatles", "British Rock");
+    assertEquals("The Beatles", testCD.getArtist());
+  }
+
+  @Test
+  public void CD_getsCorrectGenre() {
+    CD testCD = new CD("The White Album", "The Beatles", "British Rock");
+    assertEquals("British Rock", testCD.getGenre());
+  }
 }
